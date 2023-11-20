99073cdf6f3bcf6a69f97925276b9aac520bd50b
csdurkin <151471312+csdurkin@users.noreply.github.com>
2023-11-19 23:10:17 -0500

    Update csvsum.py

--- prog/csvsum.py
+++ prog/csvsum.py
@@ -1,19 +1,25 @@
+#!/usr/bin/env python3
+
 import sys
 import csv
 
 def csvsum(csv_reader_object, column):
-
+    
     column_sum = 0
     
     for row in csv_reader_object:
         
         try: 
-    
-            column_sum += float(row[column])
-
+            value = float(row[column])
+            column_sum += value
+            
         except ValueError:
             
             print(f'ValueError: {row[column]} cannot be converted to a numerical value.')
+
+        except KeyError:
+            
+            print(f'KeyError: Column "{column}" not found in the current row.')
 
     print(f'Column: {column}; Sum: {column_sum}')
 
@@ -30,11 +36,23 @@ if __name__ == '__main__':
             total_sum = 0
 
             for column in sys.argv[2:]:
+                
+                with open(file_name, 'r') as file:
+                    
+                    csv_reader_object = csv.DictReader(file)       
+                    
+                    if not csv_reader_object.fieldnames:
+                        print("CSV File is empty.")
+                        sys.exit(1)
+                    
+                    total_sum += csvsum(csv_reader_object, column)
 
-                    with open(file_name, 'r') as file:
-                        csv_reader_object = csv.DictReader(file)
-                        
-                        total_sum += csvsum(csv_reader_object, column)
+                #try:
+                    #column_index = column
+                    
+                #except ValueError:
+                    #print(f'Error: The column argument "{column}" is not a valid integer.')
+                    #sys.exit(1)
                 
             print(f'Total sum of all columns: {total_sum}')
 
@@ -45,9 +63,20 @@ if __name__ == '__main__':
             csv_reader_object = csv.DictReader(sys.stdin)
             
             total_sum = 0
-            
+
             for column in sys.argv[2:]:
                 
+                #try:
+                    #column_index = column
+                    
+                #except ValueError:
+                    #print(f'Error: The column argument "{column}" is not a valid integer.')
+                    #sys.exit(1)
+
+                if not csv_reader_object.fieldnames:
+                        print("CSV File is empty.")
+                        sys.exit(1)
+                        
                 total_sum += csvsum(csv_reader_object, column)
 
             print(f'Total sum of all columns: {total_sum}')
@@ -63,6 +92,7 @@ if __name__ == '__main__':
         sys.exit(1)    
 
     except Exception as e:
-        print(f'Error: {e}')
-        sys.exit(1)       
-        
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        print(f'Exception Error: {exc_type.__name__} - {exc_value}')
+        sys.exit(1)
+
